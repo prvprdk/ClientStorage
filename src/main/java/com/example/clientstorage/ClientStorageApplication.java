45d0f01b03f5a6ec0efa86f7a71ae3f6fd00ef30
prvprdk <prvprdk@gmail.com>
2023-10-26 18:21:04 +0500

    Initial commit

---
+++ src/main/java/com/example/clientstorage/ClientStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/clientstorage/ClientStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
